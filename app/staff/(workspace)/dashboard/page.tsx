085439375a4a66d2be433c3ccf8dbfe1d9874bff
Sir <duyhuu1109@gmail.com>
2025-12-14 11:55:34 +0700

    feat(staff): add notification panel and reshape chart

--- app/staff/(workspace)/dashboard/page.tsx
+++ app/staff/(workspace)/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import DashboardHeader from "@/components/staff/DashboardHeader";
+import NotificationPanel from "@/components/staff/NotificationPanel";
 import PatientStatisticChart from "@/components/staff/PatientStatisticChart";
 import StatsGrid from "@/components/staff/StatGrid";
 
@@ -7,7 +8,14 @@ export default function DashboardPage() {
         <div className="flex flex-col gap-6 min-h-screen px-6 py-8 bg-white">
             <DashboardHeader userName="Nguyen Huu Duy" />
             <StatsGrid />
-            <PatientStatisticChart />
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 h-[450px]">
+                <div className="lg:col-span-2">
+                    <PatientStatisticChart />
+                </div>
+                <div className="lg:col-span-1">
+                    <NotificationPanel />
+                </div>
+            </div>
         </div>
     );
 }
