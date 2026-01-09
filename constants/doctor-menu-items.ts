388a67659bee2ff79c1198c95ce87429a233234a
Sir <duyhuu1109@gmail.com>
2026-01-10 01:15:35 +0700

    chore(doctor): exclude chat with patient

--- constants/doctor-menu-items.ts
+++ constants/doctor-menu-items.ts
@@ -12,7 +12,6 @@ export const MENU_ITEMS = [
     { label: "Dashboard", href: "/dashboard", icon: LayoutDashboard },
     { label: "Schedule", href: "/schedule", icon: Bell },
     { label: "Manage Appointment", href: "/appointments", icon: Calendar },
-    { label: "Chat with patient", href: "/chat", icon: MessageSquare },
     { label: "Manage Encounter", href: "/encounters", icon: Stethoscope },
     { label: "Doctor Profile", href: "/profile", icon: Users },
 ];
