d15bf1b74e4b0acc3e12dbec2599e3be9152949d
Sir <duyhuu1109@gmail.com>
2026-01-11 15:38:40 +0700

    feat(doctor): handle check stock in prescription creating in diagnosis modal

--- constants/doctor-menu-items.ts
+++ constants/doctor-menu-items.ts
@@ -1,3 +1,4 @@
+import PrescriptionCard from "@/components/doctor/prescriptions/PrescriptionCard";
 import {
     LayoutDashboard,
     Calendar,
@@ -6,12 +7,18 @@ import {
     Bell,
     Stethoscope,
     Users,
+    PillBottle,
 } from "lucide-react";
 
 export const MENU_ITEMS = [
     { label: "Dashboard", href: "/dashboard", icon: LayoutDashboard },
     { label: "Schedule", href: "/schedule", icon: Bell },
     { label: "Manage Appointment", href: "/appointments", icon: Calendar },
+    {
+        label: "Manage Prescriptions",
+        href: "/prescriptions",
+        icon: PillBottle,
+    },
     { label: "Manage Encounter", href: "/encounters", icon: Stethoscope },
     { label: "Doctor Profile", href: "/profile", icon: Users },
 ];
